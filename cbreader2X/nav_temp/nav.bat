70908bea806feff2ed3e09b4a083742eff2cf7f4
heavenchou <heaven.chou@gmail.com>
2026-05-21 16:46:11 +0800

    nav 暫時搬到 nav_temp

---
+++ cbreader2X/nav_temp/nav.bat
@@ -0,0 +1,30 @@
+@echo off
+IF "%1"=="" call :show_help
+IF "%1"=="-h" call :show_help
+IF "%1"=="-a" call :run_all
+IF "%1"=="-see" call :run_seeland
+exit /B
+
+:show_help
+echo nav.bat -h     : help
+echo nav.bat -a     : run all
+echo nav.bat -see   : run seeland
+exit /B
+
+:run_all
+perl create_bulei_nav.pl 
+perl create_book_nav.pl simple_nav.txt
+perl create_book_nav.pl advance_nav.txt
+rem CBReader 全文檢索單經搜尋用的檔案
+perl create_book_slr.pl SutraList.txt
+cd ../gaiji2word
+perl gaiji2word.pl nav
+cd ../nav
+exit /B
+
+:run_seeland
+perl create_book_nav.pl seeland_nav.txt
+cd ../gaiji2word
+perl gaiji2word.pl nav_see
+cd ../nav
+exit /B
